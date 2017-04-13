f5345c7b42c7b821bae44acb1c1904bb49d4c77d
Perry Berman <R3alCl0ud@gmail.com>
2017-04-13 00:27:48 -0700

    start the compiler too

--- src/svm/main/main.c
+++ src/svm/main/main.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 int main() {
-  printf("YAY it's the Something-Lang VM, or the SVM for short");
+  printf("YAY it's the Something-Lang VM, or the SVM for short\n");
 
   return 0;
 }
